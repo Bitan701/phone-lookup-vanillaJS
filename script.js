5d74b4215d29aab4cf3da6daa604f0a463c69651
bitan701 <bitan701@gmail.com>
2022-03-02 09:42:48 +0600

    collapse done

--- script.js
+++ script.js
@@ -12,17 +12,17 @@ const funSearch = () => {
     .then((data) => {
       data.data.forEach((e) => {
         cardGroup.innerHTML += `
-        <div class="col">
-        <div onClick="cardDetails(\'${e.slug}\')" class="card mx-auto" style="width: 10rem">
-            <img style="img-fluid" src="${e.image}" class="card-img-top" alt="${e.phone_name}" />
-            <div class="card-body">
+          <div class="col">
+            <div onClick="cardDetails(\'${e.slug}\')" class="card mx-auto" style="width: 10rem">
+              <img style="img-fluid" src="${e.image}" class="card-img-top" alt="${e.phone_name}" />
+              <div class="card-body">
                 <h5 class="card-title">${e.phone_name}</h5>
                 <p class="card-text">
                     Brand: ${e.brand}
                 </p>
+              </div>
             </div>
-        </div>
-        </div>
+          </div>
         `
       })
     })
@@ -34,13 +34,15 @@ const cardDetails = (link) => {
   fetch(fetchDetails + link)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
-        cardDetailsGroup.innerHTML = `
-        <div class="" style="width: 15rem">
+      console.log(data)
+      btnCollapse.classList.remove('d-none')
+      cardDetailsGroup.classList.remove('d-none')
+      cardDetailsGroup.innerHTML = `
+          <div class="" style="width: 15rem">
             <img src="${data.data.image}" class="card-img-top" alt="${data.data.name}" />
             <div class="card-body">
                 <h5 class="card-title">${data.data.name}</h5>
-                  <table class="table text-wrap">
+                <table class="table text-wrap">
                   <tbody>
                     <tr>
                       <th scope="row">Brand:</th>
@@ -67,10 +69,16 @@ const cardDetails = (link) => {
                       <td colspan="2">${data.data.mainFeatures.sensors}</td>
                     </tr>
                   </tbody>
-                  </table>
+                </table>
             </div>
-        </div>
+          </div>
         `
     })
 }
 
+const hideDetails = () => {
+  cardDetailsGroup.classList.add("d-none")
+  btnCollapse.classList.add("d-none")
+}
+
+btnCollapse.addEventListener('click', hideDetails)
