aa8b09211e53699eb71cc3c06f6780bbd4ab571d
bitan701 <bitan701@gmail.com>
2022-03-02 12:24:10 +0600

    save before sizing

--- script.js
+++ script.js
@@ -5,17 +5,25 @@ const cardDetailsGroup = document.getElementById('cardDetailsGroup')
 const fetchLink = 'https://openapi.programming-hero.com/api/phones?search='
 const fetchDetails = 'https://openapi.programming-hero.com/api/phone/'
 const btnCollapse = document.getElementById('btnCollapse')
+let count = 0
 
+// Loading all the search results data.data.forEach((e) =>
 const funSearch = () => {
+  cardGroup.textContent = ""
   fetch(fetchLink + inputSearch.value)
     .then((response) => response.json())
     .then((data) => {
-      data.data.forEach((e) => {
+      for(e of data.data) {
+        count++
+        if (count == 21) {
+          count = 0
+          break
+        }
         cardGroup.innerHTML += `
-          <div class="col">
-            <div onClick="cardDetails(\'${e.slug}\')" class="card mx-auto" style="width: 10rem">
+          <div class="col my-3">
+            <div onClick="cardDetails(\'${e.slug}\')" class=" mx-auto " style="width: 10rem; cursor: pointer">
               <img style="img-fluid" src="${e.image}" class="card-img-top" alt="${e.phone_name}" />
-              <div class="card-body">
+              <div class="card card-body mt-3 p-2 rounded-3">
                 <h5 class="card-title">${e.phone_name}</h5>
                 <p class="card-text">
                     Brand: ${e.brand}
@@ -24,58 +32,69 @@ const funSearch = () => {
             </div>
           </div>
         `
-      })
+      }
+      count = 0
     })
 }
 
 btnSearch.addEventListener('click', funSearch)
 
+// Showing Details of selected card
 const cardDetails = (link) => {
   fetch(fetchDetails + link)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
+      // console.log(data)
+      scroll(0, 0)
       btnCollapse.classList.remove('d-none')
       cardDetailsGroup.classList.remove('d-none')
       cardDetailsGroup.innerHTML = `
-          <div class="" style="width: 15rem">
-            <img src="${data.data.image}" class="card-img-top" alt="${data.data.name}" />
+          <div class="d-lg-flex justify-content-lg-around align-items-lg-center" style="width: 15rem">
+            <img src="${
+              data.data.image
+            }" width= "5000" class="card-img-top border border-primary" alt="${
+        data.data.name
+      }" />
+            
             <div class="card-body">
-                <h5 class="card-title">${data.data.name}</h5>
-                <table class="table text-wrap">
-                  <tbody>
-                    <tr>
-                      <th scope="row">Brand:</th>
-                      <td>${data.data.brand}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Release Date:</th>
-                      <td>${data.data.releaseDate}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Chipset:</th>
-                      <td colspan="2">${data.data.mainFeatures.chipSet}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Display Size:</th>
-                      <td colspan="2">${data.data.mainFeatures.displaySize}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Memory:</th>
-                      <td colspan="2">${data.data.mainFeatures.memory}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Sensors:</th>
-                      <td colspan="2">${data.data.mainFeatures.sensors}</td>
-                    </tr>
-                  </tbody>
-                </table>
+              <h5 class="card-title">${data.data.name}</h5>
+              <table class="table">
+                <tbody>
+                  <tr>
+                    <th scope="row">Brand:</th>
+                    <td>${data.data.brand}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Release Date:</th>
+                    <td>${data.data.releaseDate}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Chipset:</th>
+                    <td colspan="2">${data.data.mainFeatures.chipSet}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Display Size:</th>
+                    <td colspan="2">${data.data.mainFeatures.displaySize}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Memory:</th>
+                    <td colspan="2">${data.data.mainFeatures.memory}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Sensors:</th>
+                    <td colspan="2">${data.data.mainFeatures.sensors.join(
+                      ' '
+                    )}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         `
     })
 }
 
+// Collapse details
 const hideDetails = () => {
   cardDetailsGroup.classList.add("d-none")
   btnCollapse.classList.add("d-none")
